9a686f9fb40f26ca9a730e1497ed3a0f9c9da8b8
shuchao <645904877@qq.com>
2016-06-16 16:45:40 +0800

    format

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var server = "http://121.40.190.5:8082";
-
 // todo 登录验证
 var access = function ($http) {
-    var form_data = {
+    var data = {
         time       : 1465887338,
         mac_address: '3c:46:d8:8a:50:cd'
     };
-    $http.post(server + "/groups_encrypt/services/rest/statu/bindMac/json?user_id=5705b6c4e58a3a0a2e8b459f&token=3852c90", form_data).success(function (res) {
+    $http.post("/ajax/access", data).success(function (res) {
         console.log(res);
     });
 };
