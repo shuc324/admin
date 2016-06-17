bba867c25da06c20ab50f392dee7d0fb6433f4e0
shuchao <645904877@qq.com>
2016-06-17 17:49:39 +0800

    code review

--- http/route.js
+++ http/route.js
@@ -17,10 +17,8 @@ export default class route {
 
     static asserts(app) {
         // 静态文件
-        app.express.use('/index.html', express.static(app.app_path + "/public"));
-        app.express.use('/auth.html', express.static(app.app_path + "/public"));
-        app.express.use('/404.html', express.static(app.app_path + "/public"));
-        app.express.use('/reg.html', express.static(app.app_path + "/public"));
+        app.express.use(['/index.html', '/auth.html', '/404.html', '/reg.html'], express.static(app.app_path + "/public"));
+
         app.express.use('/assets/*', express.static(app.app_path + "/public/assets"));
         app.express.use('/fonts/*', express.static(app.app_path + "/public/fonts"));
         app.express.use('/maps/*', express.static(app.app_path + "/public/maps"));
