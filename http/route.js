5a4fbe31af4eedd03656768bedab82874c70acf2
shuchao <645904877@qq.com>
2016-06-21 18:38:26 +0800

    modify static file

--- http/route.js
+++ http/route.js
@@ -16,21 +16,27 @@ export default class route {
 
     static asserts(app) {
         // 静态文件
-        app.express.use(['/', '/index.html', '/auth.html', '/404.html', '/reg.html'], express.static(app.app_path + "/public"));
+        app.express.use('/', (request, response) => {
+            response.sendFile(app.app_path + "/public/index.html");
+        });
+        app.express.use('/sign_up.html', (request, response) => {
+            response.sendFile(app.app_path + "/public/reg.html");
+        });
+        app.express.use('/sign_in.html', (request, response) => {
+            response.sendFile(app.app_path + "/public/auth.html");
+        });
+        app.express.use('/404.html', (request, response) => {
+            response.sendFile(app.app_path + "/public/404.html");
+        });
 
-        app.express.use('/assets/*', express.static(app.app_path + "/public/assets"));
-        app.express.use('/fonts/*', express.static(app.app_path + "/public/fonts"));
         app.express.use('/maps/*', express.static(app.app_path + "/public/maps"));
-        app.express.use('/scripts/*', express.static(app.app_path + "/public/scripts"));
+        app.express.use('/fonts/*', express.static(app.app_path + "/public/fonts"));
+        app.express.use('/assets/*', express.static(app.app_path + "/public/assets"));
         app.express.use('/styles/*', express.static(app.app_path + "/public/styles"));
-
+        app.express.use('/scripts/*', express.static(app.app_path + "/public/scripts"));
         // 404
         app.express.use('*', (request, response) => {
-            response.json({
-                code   : 404,
-                message: 'not found',
-                data   : {}
-            });
+            response.json({code: 404, message: 'not found', data: {}});
         });
     }
 }
