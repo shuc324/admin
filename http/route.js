6e94d5477e7af24a39edbf42914d3b9b53642841
shuchao <645904877@qq.com>
2016-06-22 11:00:13 +0800

    fix bugπ

--- http/route.js
+++ http/route.js
@@ -4,39 +4,25 @@
 "use strict";
 
 import express from "express";
+import engines from "consolidate";
 
 export default class route {
     static start(app) {
         // 定义路由
-        app.post('/user');
 
         route.asserts(app);
+
+        app.post('/user');
+
         return app.start();
     }
 
     static asserts(app) {
         // 静态文件
-        app.express.use('/', (request, response) => {
-            response.sendFile(app.app_path + "/public/index.html");
-        });
-        app.express.use('/sign_up.html', (request, response) => {
-            response.sendFile(app.app_path + "/public/reg.html");
-        });
-        app.express.use('/sign_in.html', (request, response) => {
-            response.sendFile(app.app_path + "/public/auth.html");
-        });
-        app.express.use('/404.html', (request, response) => {
-            response.sendFile(app.app_path + "/public/404.html");
-        });
-
-        app.express.use('/maps/*', express.static(app.app_path + "/public/maps"));
-        app.express.use('/fonts/*', express.static(app.app_path + "/public/fonts"));
-        app.express.use('/assets/*', express.static(app.app_path + "/public/assets"));
-        app.express.use('/styles/*', express.static(app.app_path + "/public/styles"));
-        app.express.use('/scripts/*', express.static(app.app_path + "/public/scripts"));
+        app.express.use('/', express.static("public"));
         // 404
-        app.express.use('*', (request, response) => {
-            response.json({code: 404, message: 'not found', data: {}});
-        });
+        //app.express.use('*', (request, response) => {
+        //    response.json({code: 404, message: 'not found', data: {}});
+        //});
     }
 }
